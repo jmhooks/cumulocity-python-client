d6c77038cb8cf6d3223eb97ac50a85703524a042
Josh Hooks <josh.hooks@outlook.com>
2018-05-22 01:27:26 -0400

    software update

--- client/main.py
+++ client/main.py
@@ -9,37 +9,49 @@ import os
 
 import util.device as device
 import util.location as location
+import util.logger as logger
+
+log = logger.get_logger(__name__)
 
 receivedMessages = []
-device.print_env_values()
+#device.print_env_values()
 
 count = 0
 
 def on_message(client, userdata, message):
-    print("Received operation " + str(message.payload))
+    log.info("Received operation " + str(message.payload))
     if message.payload.startswith("510"):
-        print("Simulating device restart...")
         publish("s/us", "501,c8y_Restart")
-        print("...restarting...")
-        time.sleep(1)
+        log.info("...restarting...")
         publish("s/us", "503,c8y_Restart")
-        print("...done...")
+        os.system('reboot')
+    if message.payload.startswith("511"):
+        publish("s/us", "501,c8y_Command")
+        command = str(message.payload.split(',')[2])
+        log.info("Received command: " + command)
+        output = str(os.popen(command).read()[:200])
+        log.info("Output: " + output)
+        publish("s/us", "503,c8y_Command,\"" + output + "\"")
+    if message.payload.startswith("516"):
+        publish("s/us", "501,c8y_Software")
+        url = str(message.payload.split(',')[4])
+        log.info("Downloading software from URL: " + url)
+        os.system('/home/pi/cumulocity-python-client/install.sh')
+        publish("s/us", "503,c8y_Software")
 
 
 def send_measurements():
     global count
     try:
-        print("Sending power measurement")
         #power1 = subprocess.check_output("cat /var/log/emonhub/emonhub.log | grep 'power1 ' | tail -n 1 | awk '{print $7}'", shell=True).strip()
         power1 = str(os.popen("cat /var/log/emonhub/emonhub.log | grep 'power1 ' | tail -n 1 | awk '{print $7}'").read()).strip()
-        print "Power: " + str(power1)
+        log.info("Power: " + str(power1))
         publish("s/us", "200,c8y_CurrentSensor,T," + str(power1))
-        print("Sending signal strength measurement")
-        #signal = subprocess.check_output("iwconfig wlan0 | grep 'Link Quality' | awk '{print substr($2, 9)}'", shell=True).strip()
-        signal = str(os.popen("iwconfig wlan0 | grep 'Link Quality' | awk '{print substr($2, 9)}'").read()).strip()
+        #signal = subprocess.check_output("/sbin/iwconfig wlan0 | grep 'Link Quality' | awk '{print substr($2, 9)}'", shell=True).strip()
+        signal = str(os.popen("/sbin/iwconfig wlan0 | grep 'Link Quality' | awk '{print substr($2, 9)}'").read()).strip()
         signal_split = signal.split('/')
         signal_total = int(signal_split[0]) / int(signal_split[1]) * 100 
-        print "Signal Strength: " + str(signal_total)
+        log.info("Signal Strength: " + str(signal_total))
         publish("s/us", "210," + str(signal_total))
         thread = threading.Timer(5, send_measurements)
         thread.daemon = True
@@ -49,7 +61,7 @@ def send_measurements():
             while True:
                 time.sleep(100)
     except (KeyboardInterrupt, SystemExit):
-        print 'Received keyboard interrupt, quitting ...'
+        log.info('Received keyboard interrupt, quitting ...')
 
 
 def publish(topic, message, wait_for_ack=False):
@@ -72,7 +84,9 @@ client.connect(device.get_server_host(), 1883)
 client.loop_start()
 publish("s/us", "100," + device.get_client_id() + ",c8y_MQTTAgent", True)
 publish("s/us", "110," + device.get_client_id() + "," + device.get_client_model() + ",Rev0.1")
-print "Location: " + str(location.get_lat_long())
+log.info("Location: " + str(location.get_lat_long()))
 publish("s/us", "112," + str(location.get_lat_long()))
+publish("s/us", "114,c8y_Restart,c8y_Configuration,c8y_SoftwareList,c8y_Software,c8y_Firmware,c8y_LogfileRequest,c8y_Command")
+publish("s/us", "400,c8y_ConnectionEvent,'Device connected'")
 client.subscribe("s/ds")
 send_measurements()
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /home/pi/cumulocity-python-client
+/usr/bin/git pull
+sudo /usr/bin/python setup.py install
+reboot
